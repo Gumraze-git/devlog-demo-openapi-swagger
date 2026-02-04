37b22c364f78c76e128d0543399d1581fe682a93
gumraze-git <galaxydh4110@gmail.com>
2026-02-04 19:44:51 +0900

    first commit

---
+++ src/test/java/com/gumraze/opanapi/OpenapiSwaggerDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gumraze.opanapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenapiSwaggerDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
